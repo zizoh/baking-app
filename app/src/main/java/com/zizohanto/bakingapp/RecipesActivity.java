59fcd386235e468b1b9e970f605cf322022a11a8
Zizoh Anto <zizohanto@hotmail.com>
2018-10-27 00:34:33 +0100

    initial commit

---
+++ app/src/main/java/com/zizohanto/bakingapp/RecipesActivity.java
@@ -0,0 +1,13 @@
+package com.zizohanto.bakingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipes_act);
+    }
+}
